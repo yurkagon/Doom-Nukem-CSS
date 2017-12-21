98fd0d5a91b57027a6c2e2a07a367c18c219176f
Yuragon <owngameplay@gmail.com>
2017-12-21 11:07:58 +0200

    Changed sound volume

--- script/app.js
+++ script/app.js
@@ -20,6 +20,7 @@ $(document).ready(() =>{
 
 	var mainThemeMusic = new Audio('sounds/main_theme.mp3');
 	mainThemeMusic.loop = true;
+	mainThemeMusic.volume = 0.3;
 	mainThemeMusic.play();
 
 	//control
