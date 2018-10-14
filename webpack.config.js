121487186dfa6bb0fedb0b787d76f88491239372
yurkagon <owngameplay@gmail.com>
2018-10-14 15:25:33 +0300

    [ADD] devserver

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const SassPlugin = require('sass-webpack-plugin');
+
+const buildFolder = 'build';
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'build')
+    path: path.resolve(__dirname, buildFolder)
   },
   plugins: [
+    new CleanWebpackPlugin([buildFolder]),
     new CopyWebpackPlugin([
       { from: './public', to: './' }
     ]),
@@ -17,5 +21,10 @@ module.exports = {
       sass: { outputStyle: 'compressed' },
       autoprefixer: false
     }),
-  ]
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, buildFolder),
+    compress: true,
+    port: 9000,
+  }
 };
