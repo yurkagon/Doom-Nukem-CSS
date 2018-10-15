762904c520232fbe20364df2a7c344d14a2078e9
yurkagon <owngameplay@gmail.com>
2018-10-15 04:02:13 +0300

    [ADD] walking state

--- src/classes/Sprite/Enemy/index.js
+++ src/classes/Sprite/Enemy/index.js
@@ -1,9 +1,21 @@
 import Sprite from '../index';
 
 class Enemy extends Sprite {
+  states = {
+    DEFAULT: 'default',
+    DEAD: 'dead',
+    WALK: 'walk'
+  }
+  currenState = null;
 	constructor(type, x, z, y=200) {
-		super(type, x, y, z, 'enemy');
-	}
+    super(type, x, y, z, 'enemy');
+    this.setState(this.states.WALK);
+  }
+  setState(state) {
+    this.self.removeClass(this.currenState);
+    this.self.addClass(state);
+    this.currenState = state;
+  }
 }
 
 export default Enemy;
