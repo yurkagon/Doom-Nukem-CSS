5640c540c15c4011e5c3b8ffe2c1b9de7fb3073b
yurkagon <owngameplay@gmail.com>
2018-10-15 20:06:53 +0300

    [ADD] changing states logic

--- src/classes/Sprite/Enemy/index.js
+++ src/classes/Sprite/Enemy/index.js
@@ -1,7 +1,16 @@
 import Sprite from '../index';
+import { Distance } from '../../../helpers';
+
+const LOGIC_INTERVAL = 500;
+
 
 class Enemy extends Sprite {
-  states = {
+  static LOGIC_INTERVAL = 500;
+  static VISION_DISTANCE = 2500;
+  static MAX_WALKING_TO_PLAYER_DISTANCE = 3100;
+  static ATACK_DISTANCE = 1000;
+
+  static states = {
     DEFAULT: 'default',
     DEAD: 'dead',
     WALK: 'walk',
@@ -9,10 +18,48 @@ class Enemy extends Sprite {
     ATACK: 'atack'
   }
   currenState = null;
+  timer = null;
 
 	constructor(type, x, z, y = 200) {
     super(type, x, y, z, 'enemy');
-    this.setState(this.states.ATACK);
+    this.logicUpdate = this.logicUpdate.bind(this);
+
+    this.setState(Enemy.states.DEFAULT);
+    this.timer = setInterval(this.logicUpdate, Enemy.LOGIC_INTERVAL);
+  }
+  logicUpdate() {
+    const {
+      VISION_DISTANCE,
+      MAX_WALKING_TO_PLAYER_DISTANCE,
+      ATACK_DISTANCE,
+      states
+    } = Enemy;
+
+    const distance = Distance(this.getPosition(), window.player.getPosition());
+
+    switch(this.currenState) {
+      case states.DEFAULT:
+        if(distance <= ATACK_DISTANCE) {
+          this.setState(states.ATACK);
+        } else if(distance <= VISION_DISTANCE) {
+          this.setState(states.WALK);
+        }
+        break;
+      case states.WALK:
+        if(distance <= ATACK_DISTANCE) {
+          this.setState(states.ATACK);
+        } else if(distance >= MAX_WALKING_TO_PLAYER_DISTANCE) {
+          this.setState(states.DEFAULT);
+        }
+        break;
+      case states.ATACK:
+        if(distance >= ATACK_DISTANCE) {
+          this.setState(states.WALK);
+        }
+      default:
+        return;
+
+    }
   }
   setState(state) {
     this.self.removeClass(this.currenState);
