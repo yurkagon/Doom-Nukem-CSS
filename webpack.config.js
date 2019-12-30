2f889756cdbdf3ca32a0fe0fbee07d5c420ee41e
yurkagon <owngameplay@gmail.com>
2019-12-30 08:57:53 +0200

    updated dev dependenices

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,45 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
-const buildFolder = 'build';
+const buildFolder = "build";
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: "./src/index.ts",
   output: {
-    filename: 'main.js',
+    filename: "main.js",
     path: path.resolve(__dirname, buildFolder)
   },
   plugins: [
-    new CleanWebpackPlugin([buildFolder]),
-    new CopyWebpackPlugin([
-      { from: './public', to: './' }
-    ]),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([{ from: "./public", to: "./" }])
   ],
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/
       },
       {
-      test: /\.scss$/,
-      use: [
-        {
-          loader: "style-loader"
-        },
-        {
-          loader: "css-loader?-url"
-        },
-        {
-          loader: "sass-loader",
-        }
-      ]
-    }]
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader?-url"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      }
+    ]
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
+    extensions: [".tsx", ".ts", ".js"]
   },
   optimization: {
     minimizer: [
@@ -56,6 +55,6 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, buildFolder),
     compress: true,
-    port: 9000,
+    port: 9000
   }
 };
