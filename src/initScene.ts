0616c3fab5bacc337820a611ea5ae568774a1fbb
yurkagon <owngameplay@gmail.com>
2020-01-11 04:16:25 +0200

    [ADD] enemy collision

--- src/initScene.ts
+++ src/initScene.ts
@@ -57,10 +57,10 @@ export default async () => {
 };
 
 const setLevel = () => {
-  // new ShotgunItem({
-  //   x: 1000,
-  //   z: 1000
-  // });
+  new ShotgunItem({
+    x: 1000,
+    z: 1000
+  });
   for (let i = 0; i < 5; i++)
     new House({
       position: {
@@ -69,12 +69,12 @@ const setLevel = () => {
         z: -3000
       }
     });
-  // for (let i = 0; i < 30; i++)
-  //   new Enemy({
-  //     type: "guard",
-  //     position: {
-  //       x: 1000 + i * 100,
-  //       z: 1000 + i * 100
-  //     }
-  //   });
+  for (let i = 0; i < 30; i++)
+    new Enemy({
+      type: "guard",
+      position: {
+        x: 1000 + i * 100,
+        z: 1000 + i * 100
+      }
+    });
 };
