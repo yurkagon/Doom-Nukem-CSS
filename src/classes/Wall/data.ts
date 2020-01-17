65d40716b298d4b6532d46786bb15a495827319d
yurkagon <owngameplay@gmail.com>
2020-01-17 22:39:46 +0200

    update css to scss

--- src/classes/Wall/data.ts
+++ src/classes/Wall/data.ts
@@ -1,20 +1,17 @@
 export default `
+  <div class="shape cuboid back">
+    <div class="face bk"></div>
+  </div>
 
-<div class="shape cuboid-25 back">
-  <div class="face bk"></div>
-</div>
+  <div class="shape cuboid front">
+    <div class="face ft"></div>
+  </div>
 
-<div class="shape cuboid-26 front">
-  <div class="face ft"></div>
-</div>
+  <div class="shape cuboid right">
+    <div class="face lt"></div>
+  </div>
 
-<div class="shape cuboid-29 right">
-  <div class="face lt"></div>
-</div>
-
-<div class="shape cuboid-30 left">
-
-<div class="face rt"></div>
-
-</div>
+  <div class="shape cuboid left">
+    <div class="face rt"></div>
+  </div>
 `;
