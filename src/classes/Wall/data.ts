09b54222b1155c7afaac2c9b12c11d9dbb9bb107
yurkagon <owngameplay@gmail.com>
2020-01-17 22:22:14 +0200

    wip block

--- src/classes/Wall/data.ts
+++ src/classes/Wall/data.ts
@@ -9,19 +9,12 @@ export default `
 </div>
 
 <div class="shape cuboid-29 right">
-<div class="face ft"></div>
-<div class="face bk"></div>
-<div class="face rt"></div>
-<div class="face lt"></div>
-<div class="face bm"></div>
-<div class="face tp"></div>
+  <div class="face lt"></div>
 </div>
+
 <div class="shape cuboid-30 left">
-<div class="face ft"></div>
-<div class="face bk"></div>
+
 <div class="face rt"></div>
-<div class="face lt"></div>
-<div class="face bm"></div>
-<div class="face tp"></div>
+
 </div>
 `;
