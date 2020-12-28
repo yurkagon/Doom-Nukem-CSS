3754994a3ba945b616d468db800509189048bbfd
yurkagon <owngameplay@gmail.com>
2020-12-28 04:30:24 +0200

    Updated level select screen

--- src/ui/screens/LevelSelect/LevelSelect.tsx
+++ src/ui/screens/LevelSelect/LevelSelect.tsx
@@ -1,31 +1,29 @@
 import React, { Component } from "react";
 
-import sleep from "utils/sleep";
-
-import Level from "classes/Level";
+import Level, { LevelName } from "classes/Level";
 
 import ScreenWrapper from "ui/components/Screen";
-
 import ButtonGroup from "ui/components/ButtonGroup";
-import { ButtonData } from "ui/components/ButtonGroup/types";
 
 import "./style.scss";
 
 class LevelSelect extends Component {
-  private buttonsData: ButtonData[] = [
-    {
-      text: "level_1",
-      onClick: () => {
-        Level.load("level_1");
-      }
-    }
-  ];
+  private levelNames: LevelName[] = ["level_1"];
 
   public render() {
     return (
       <ScreenWrapper className="menu">
         <div className="navigation">
-          {<ButtonGroup data={this.buttonsData} />}
+          {
+            <ButtonGroup
+              data={this.levelNames.map(levelName => ({
+                text: levelName,
+                onClick: () => {
+                  Level.load(levelName);
+                }
+              }))}
+            />
+          }
         </div>
       </ScreenWrapper>
     );
