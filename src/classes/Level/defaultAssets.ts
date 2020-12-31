3986a4f53b8048badea8bac36d2db85722eda4ac
yurkagon <owngameplay@gmail.com>
2020-12-31 04:37:54 +0200

    weapon

---
+++ src/classes/Level/defaultAssets.ts
@@ -0,0 +1,18 @@
+import { ResourcesData } from "State";
+
+const data: ResourcesData = {
+  images: [
+    "img/weapons/shotgun.png",
+    "img/enemy/guard.png",
+    "img/items/medkit.png",
+    "img/items/shotgun.png",
+    "img/hud/face.png"
+  ],
+  sounds: [
+    "sounds/voice/medkit.wav",
+    "sounds/items/itemPickUp.wav",
+    "sounds/items/pickWeapon.wav"
+  ]
+};
+
+export default data;
--- src/levels/level_1/preloadData.ts
+++ src/levels/level_1/preloadData.ts
@@ -3,12 +3,8 @@ import { ResourcesData } from "State";
 const preloadData: ResourcesData = {
   images: [
     "img/skybox.jpg",
-    "img/wall.jpg",
-    "img/weapon.png",
-    "img/enemy/guard.png",
-    "img/items/medkit.png",
-    "img/items/shotgun.png",
     "img/grass.jpg",
+    "img/floor.png",
     "img/models/house/sidingTexture.jpg",
     "img/models/house/Front.png",
     "img/models/house/Side.png"
@@ -16,8 +12,6 @@ const preloadData: ResourcesData = {
   sounds: [
     "sounds/start.wav",
     "sounds/voice/medkit.wav",
-    "sounds/items/itemPickUp.wav",
-    "sounds/items/pickWeapon.wav",
     "sounds/music/doom_e1m1.mp3"
   ]
 };
