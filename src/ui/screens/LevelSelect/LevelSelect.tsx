8e9f030cb20ec334b05bde59109416da45a539f1
yurkagon <owngameplay@gmail.com>
2021-01-06 12:41:00 +0200

    Added Header

--- src/ui/screens/LevelSelect/LevelSelect.tsx
+++ src/ui/screens/LevelSelect/LevelSelect.tsx
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import Level, { LevelName } from "classes/Level";
 
 import ScreenWrapper from "ui/components/Screen";
+import Header from "ui/components/Header";
 import ButtonGroup from "ui/components/ButtonGroup";
 import BackButton from "ui/components/BackButton";
 
@@ -15,6 +16,8 @@ class LevelSelect extends Component {
     return (
       <ScreenWrapper className="menu">
         <div className="navigation">
+          <Header>Select level</Header>
+
           {
             <ButtonGroup
               data={this.levelNames.map(levelName => ({
