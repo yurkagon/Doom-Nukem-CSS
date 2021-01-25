c5bd604df1661952d6625d952b549ffb437ff4f6
yurkagon <owngameplay@gmail.com>
2021-01-25 07:00:48 +0200

    Updates

--- 2.main.js
+++ 2.main.js
@@ -1,1 +1,1 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{44:function(n,t,u){"use strict";u.r(t);var e=u(68);u.d(t,"default",function(){return e.default})}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{44:function(n,t,u){"use strict";u.r(t);var e=u(86);u.d(t,"default",function(){return e.default})}}]);
